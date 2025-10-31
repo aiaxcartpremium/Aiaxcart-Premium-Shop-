67d718e116595cb8a92dfd4047d3a6341d725086
aiaxcartpremium <shanaiamau99@gmail.com>
2025-10-31 15:49:22 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+export const supabase = createClient(
+  'https://rvwahejdkijdigsxnjzk.supabase.co',   // ← replace
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2d2FoZWpka2lqZGlnc3huanprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4OTEwMDQsImV4cCI6MjA3NzQ2NzAwNH0.sNl2rGAM_Ofx6kp7rNA3z_S_9uOWFd4r-yxJc0c4UHg'                    // ← replace
+);
